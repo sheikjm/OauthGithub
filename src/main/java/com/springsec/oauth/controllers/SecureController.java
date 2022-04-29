17ef30275ac9ff1755d802ac2ea772596a73035f
sheik <sheik@10decoders.in>
2022-04-29 10:36:50 +0530

    first commit

---
+++ src/main/java/com/springsec/oauth/controllers/SecureController.java
@@ -0,0 +1,19 @@
+package com.springsec.oauth.controllers;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecureController {
+
+    @GetMapping("/")
+    public String main(OAuth2AuthenticationToken token) {
+        System.out.println(token.getPrincipal());
+        return "secure.html";
+    }
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome to this world";
+    }
+}
